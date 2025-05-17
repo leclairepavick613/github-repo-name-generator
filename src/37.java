f01bc969245f96a621acf023aca2137c39424831
leclairepavick613 <leclairepavick613@gmail.com>
2025-05-17 04:11:19 +0000

    Add 37.java

---
+++ src/37.java
@@ -0,0 +1,7 @@
+public class GitHubRepoNameGenerator {
+    public static void main(String[] args) {
+        // Example: Generate a random name for a Java project
+        String projectName = "MyProject-123";
+        System.out.println("Generated Github repo name: " + projectName);
+    }
+}
