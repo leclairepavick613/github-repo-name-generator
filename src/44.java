2acb081ae2a51be7b7aff6845b81a20b178868c8
leclairepavick613 <leclairepavick613@gmail.com>
2025-05-26 15:24:03 +0000

    Add 44.java

---
+++ src/44.java
@@ -0,0 +1,10 @@
+public class ProjectGenerator {
+    public static void main(String[] args) {
+        // Code to generate a GitHub project name
+        String projectName = "MyProjectName";
+        String description = "This is a sample Java project for testing purposes.";
+
+        System.out.println("Generated Github Project Name: " + projectName);
+        System.out.println("Generated Description: " + description);
+    }
+}
